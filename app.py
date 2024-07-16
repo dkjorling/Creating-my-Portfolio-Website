6b6eebb1401c8b90e9cf035ace40b40f86951c0e
Dylan Jorling <dkjorling@gmail.com>
2024-07-16 12:56:57 -0700

    update .gitignore again

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from dash import Dash, html, page_container
 # my imports
 
 # colors i like: powder blue: #63acff turq: #35fed6
-# colors cont: dark gray: #004640, grbr: #666600
+# colors cont: dark gray: #004640, grbr: #6666004
 # browns: redbr: #ae5000, tan:#deb887,
 # lights: linen: #faf0e6, darker
 ################################################################################################################################
 
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY])
+app = Dash(__name__, use_pages=True, assets_folder='assets', external_stylesheets=[dbc.themes.FLATLY])
 
 #application = app.server # added for amazon beanstalk
 
